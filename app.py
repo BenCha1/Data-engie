68d5d5df4eac05d309d4ad7e95cfdae8f7b32db4
BNTCHRT <118256618+BenCha1@users.noreply.github.com>
2022-11-15 11:33:46 +0100

    update

--- app.py
+++ app.py
@@ -4,10 +4,33 @@ import pandas as pd
 
 app =  Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def helloworld():
-    req = pd.read_csv("raw_final.csv")
+
+@app.route('/raw/<ticker>', methods=['GET'])
+def raw_ticker(ticker):
+    req = pd.read_csv("raw_final_true")
+    data = { "data": req[[str(ticker), "Date"]].to_dict(orient = "records")} #df.to_dict(orient = "records")
+    return jsonify(data)
+
+
+@app.route('/raw', methods=['GET'])
+def raw():
+    req = pd.read_csv("raw_final_true")
     data = { "data": req.to_dict(orient = "records")} #df.to_dict(orient = "records")
+    return jsonify(data)
+
+
+
+@app.route('/modified', methods=['GET'])
+def modified():
+    req = pd.read_csv("modified_true.csv")
+    data = { "data": req.to_dict(orient = "records")} #df.to_dict(orient = "records")
+    return jsonify(data)
+
+
+@app.route('/modified/<ticker>', methods=['GET'])
+def modified_ticker(ticker):
+    req = pd.read_csv("modifier_true.csv")
+    data = { "data": req[[str(ticker), "stats"]].to_dict(orient = "records")} #df.to_dict(orient = "records")
     return jsonify(data)
 
 
