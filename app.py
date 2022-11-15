1e225f95512fb9c01a8c3af8dcc0d7ea3c7a796a
BNTCHRT <118256618+BenCha1@users.noreply.github.com>
2022-11-15 11:42:12 +0100

    dq

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app =  Flask(__name__)
 
 @app.route('/raw/<ticker>', methods=['GET'])
 def raw_ticker(ticker):
-    req = pd.read_csv("raw_final_true")
+    req = pd.read_csv("raw_final_true.csv")
     data = { "data": req[[str(ticker), "Date"]].to_dict(orient = "records")} #df.to_dict(orient = "records")
     return jsonify(data)
 
 
 @app.route('/raw', methods=['GET'])
 def raw():
-    req = pd.read_csv("raw_final_true")
+    req = pd.read_csv("raw_final_true.csv")
     data = { "data": req.to_dict(orient = "records")} #df.to_dict(orient = "records")
     return jsonify(data)
 
@@ -29,7 +29,7 @@ def modified():
 
 @app.route('/modified/<ticker>', methods=['GET'])
 def modified_ticker(ticker):
-    req = pd.read_csv("modifier_true.csv")
+    req = pd.read_csv("modified_true.csv")
     data = { "data": req[[str(ticker), "stats"]].to_dict(orient = "records")} #df.to_dict(orient = "records")
     return jsonify(data)
 
